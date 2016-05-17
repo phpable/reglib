55107b263679ac6a15034a28298d16db41d6ba47
hacpaka <hacpaka@eggbe.com>
2016-05-18 01:55:46 +0300

    +

---
+++ src/Reglib.php
@@ -0,0 +1,16 @@
+<?php
+namespace Eggbe\Reglib;
+
+use \Eggbe\Prototype\TUnclonable;
+use \Eggbe\Prototype\TUncreatable;
+
+class Reglib {
+	use TUnclonable;
+	use TUncreatable;
+
+	/**
+	 * @const string
+	 */
+	const QUOTED = '\'(?:\\\\\'|[^\'])*\'|"(?:\\\\"|[^"])*"';
+
+}
