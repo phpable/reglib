faa70b1e1c190f46a6ef97da228847a474ec2d36
hacpaka <hacpaka@eggbe.com>
2018-06-02 16:28:40 +0300

    the generator-based split method added

--- src/Regexp.php
+++ src/Regexp.php
@@ -6,19 +6,19 @@ class Regexp {
 	/**
 	 * @var null
 	 */
-	private $condition = null;
+	private $pattern = null;
 
 	/**
 	 * Regexp constructor.
-	 * @param string $condition
+	 * @param string $pattern
 	 * @throws \Exception
 	 */
-	public final function __construct(string $condition){
-		if (@preg_match($condition, null) === false){
+	public final function __construct(string $pattern){
+		if (@preg_match($pattern, null) === false){
 			throw new \Exception(preg_replace('/^[^:]+:\s*/', '', error_get_last()['message']));
 		}
 
-		$this->condition = $condition;
+		$this->pattern = $pattern;
 	}
 
 	/**
@@ -27,7 +27,7 @@ class Regexp {
 	 * @return string
 	 */
 	public final function take(string $source, int $position = 0): string {
-		return preg_match($this->condition, $source, $Matches) !== false
+		return preg_match($this->pattern, $source, $Matches) !== false
 			&& isset($Matches[$position]) ? $Matches[$position] : '';
 	}
 
@@ -36,7 +36,7 @@ class Regexp {
 	 * @return bool
 	 */
 	public final function check(string $source): bool {
-		return preg_match($this->condition, $source) > 0;
+		return preg_match($this->pattern, $source) > 0;
 	}
 
 	/**
@@ -47,6 +47,27 @@ class Regexp {
 	 */
 	public final function replace(string $source, $replacement, int $limit = -1): string{
 		return is_callable($replacement) ? preg_replace_callback('', $replacement,
-			$source, $limit) : preg_replace($this->condition, $replacement, $source, $limit);
+			$source, $limit) : preg_replace($this->pattern, $replacement, $source, $limit);
+	}
+
+	/**
+	 * @param string $source
+	 * @return \Generator
+	 */
+	public final function split(string $source): \Generator {
+		$offset = 0;
+
+		while(preg_match($this->pattern, $source,
+			$Matches, PREG_OFFSET_CAPTURE, $offset) > 0){
+
+				$line  = substr($source, $offset, $Matches[0][1] - $offset);
+				$offset = $Matches[0][1] + strlen($Matches[0][0]);
+
+			yield $line;
+		}
+
+		if ($offset < strlen($source)){
+			yield substr($source, $offset);
+		}
 	}
 }
